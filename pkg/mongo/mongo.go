5e0c13186ad1e13325f4f5713f7fbab115352d4c
vissapra <rajendra@gmail.com>
2018-01-20 18:52:41 -0500

    Added test

--- pkg/mongo/mongo.go
+++ pkg/mongo/mongo.go
@@ -1,24 +1,26 @@
 package mongo
 
 import (
-	"gopkg.in/mgo.v2"
 	"time"
+
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
+
 type (
-	Mongo struct {}
+	Mongo       struct{}
 	MongoConfig struct {
-		Addrs []string
-		Timeout  time.Duration
-		Username ,Password  string
+		Addrs              []string
+		Timeout            time.Duration
+		Username, Password string
 	}
 	CollectionClient struct {
 		db, collection string
 		*mgo.Session
 	}
-	)
+)
 
-func NewMongoCache(mConfig MongoConfig) (*mgo.Session, error){
+func NewMongoCache(mConfig MongoConfig) (*mgo.Session, error) {
 	info := &mgo.DialInfo{
 		Addrs:    mConfig.Addrs,
 		Timeout:  60 * time.Second,
@@ -28,15 +30,15 @@ func NewMongoCache(mConfig MongoConfig) (*mgo.Session, error){
 	return mgo.DialWithInfo(info)
 }
 
-func NewCollectionClient(db, collection string, session *mgo.Session) *CollectionClient{
-	return &CollectionClient{db:db,collection:collection, Session: session}
+func NewCollectionClient(db, collection string, session *mgo.Session) *CollectionClient {
+	return &CollectionClient{db: db, collection: collection, Session: session}
 }
 func (m *CollectionClient) Get(key string) (response []byte, err error) {
 	session := m.Session.Copy()
 	defer session.Close()
 	collection := session.DB(m.db).C(m.collection)
 	content := struct {
-		Id string
+		Id       string
 		Response []byte
 	}{}
 	err = collection.Find(bson.M{"id": key}).One(&content)
@@ -46,11 +48,14 @@ func (m *CollectionClient) Get(key string) (response []byte, err error) {
 	return content.Response, nil
 }
 
+/*
+ *
+ */
 func (m *CollectionClient) Put(key string, value []byte) (success bool, err error) {
 	session := m.Session.Copy()
 	defer session.Close()
 	collection := session.DB(m.db).C(m.collection)
-	_, err = collection.Upsert(bson.M{"id":key},bson.M{"id": key, "response": value})
+	_, err = collection.Upsert(bson.M{"id": key}, bson.M{"id": key, "response": value})
 	return err == nil, err
 }
 func (m *CollectionClient) Exists(key string) (found bool, err error) {
@@ -60,9 +65,9 @@ func (m *CollectionClient) Exists(key string) (found bool, err error) {
 	if n, err := collection.Find(bson.M{"id": key}).Count(); n > 0 {
 		return true, nil
 	} else {
-		return false,err
+		return false, err
 	}
 }
 func (m *CollectionClient) PutWithExpiry(key string, value []byte, expiry int32) (success bool, err error) {
-	return true,nil
+	return true, nil
 }
--- pkg/mongo/mongo_test.go
+++ pkg/mongo/mongo_test.go
@@ -1,28 +1,27 @@
 package mongo
 
 import (
-	"testing"
 	"log"
+	"testing"
 )
 
 func TestCollectionClient_Put(t *testing.T) {
 	mongoCache, e := NewMongoCache(MongoConfig{Addrs: []string{"localhost:27017"}})
-	if e != nil{
+	if e != nil {
 		t.Error(e)
 	}
 	arsReqClient := NewCollectionClient("super_session", "ars_request", mongoCache)
 	success, err := arsReqClient.Put("208", []byte("resolveAvailability"))
-	if err != nil{
+	if err != nil {
 		t.Fatal(err)
 	}
-	if success{
-		if response, err := arsReqClient.Get("208") ; err !=nil{
+	if success {
+		if response, err := arsReqClient.Get("208"); err != nil {
 			t.Error(err)
 
 		} else {
 			log.Println("Response;", string(response))
 		}
-
 
 	}
 }
